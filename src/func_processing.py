e75a1bcf70fe06348b51732c0fef71ba019023d1
ochazuke1147 <ochazuke1147@gmail.com>
2019-11-06 03:19:07 +0900

    U-Net maskingを実装.

--- src/func_processing.py
+++ src/func_processing.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from src.unet import *
 
 
 def high_boost_filter(gray_image):
@@ -13,10 +14,50 @@ def high_boost_filter(gray_image):
 
     gray_image = np.uint8(gray_image)
 
-    #cv2.equalizeHist(gray_image, gray_image)
-    cv2.filter2D(gray_image, -1, kernel, gray_image)
+    # TODO: 指領域に対してのみヒストグラム平坦化処理を行うように変更する
+    cv2.equalizeHist(gray_image, gray_image)
+    boosted_image = cv2.filter2D(gray_image, -1, kernel)
+    median_image = cv2.medianBlur(boosted_image, 5)
 
-    cv2.imshow('', gray_image)
-    cv2.waitKey(0)
+    return median_image
 
-    print(kernel)
+
+def unet_masking(gray_image):
+    size = (gray_image.shape[1], gray_image.shape[0])
+    images = np.zeros((1, IMAGE_SIZE, IMAGE_SIZE, 1), np.float32)
+    image = cv2.resize(gray_image, (IMAGE_SIZE, IMAGE_SIZE))
+    image = image[:, :, np.newaxis]
+    images[0] = normalize_x(image)
+
+    input_channel_count = 1
+    output_channel_count = 1
+    first_layer_filter_count = 64
+    network = UNet(input_channel_count, output_channel_count, first_layer_filter_count)
+    model = network.get_model()
+    model.load_weights('unet_weights.hdf5')
+    BATCH_SIZE = 1
+
+    Y_pred = model.predict(images, BATCH_SIZE)
+
+    y = cv2.resize(Y_pred[0], size)
+
+    y_dn = denormalize_y(y)
+
+    y_dn = np.uint8(y_dn)
+
+    ret, mask = cv2.threshold(y_dn, 0, 255, cv2.THRESH_OTSU)
+
+    masked = cv2.bitwise_and(gray_image, mask)
+
+    mask_rest = cv2.bitwise_not(mask)
+
+    masked = cv2.bitwise_or(masked, mask_rest)
+
+    masked = masked[100:700, 100:700]
+
+    boosted = high_boost_filter(masked)
+
+    cv2.imshow('', boosted)
+
+    #cv2.waitKey(0)
+
--- src/main2.py
+++ src/main2.py
@@ -3,6 +3,22 @@ from src.func_processing import *
 from src.loader import *
 
 
-X_test, file_names = load_x('datasets' + os.sep + 'test' + os.sep + 'image', False)
+cap = cv2.VideoCapture('./datasets/movie/okazawa8.avi')
 
-high_boost_filter(X_test[0])
+print(cap.isOpened())
+
+window_name = 'image'
+
+while True:
+    ret, frame = cap.read()
+    if ret:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        unet_masking(frame)
+        #frame = high_boost_filter(frame)
+        #cv2.imshow(window_name, frame)
+        if cv2.waitKey(0) == ord('q'):
+            break
+    else:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+cv2.destroyWindow(window_name)
