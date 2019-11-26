9b4a28146cbc5866dc9d9c862ae4e1c237d2cbe5
ochazuke1147 <ochazuke1147@gmail.com>
2019-11-26 18:27:16 +0900

    SegNet関連を実装.

--- src/main_segnet.py
+++ src/main_segnet.py
@@ -1,3 +1,7 @@
 from src.segnet import *
 
 network = segnet(1, 1, 64)
+network.summary()
+train_segnet()
+
+segnet_predict()
--- src/segnet.py
+++ src/segnet.py
@@ -3,7 +3,13 @@ from keras.layers.core import Activation, Flatten, Reshape
 from keras.layers.convolutional import Convolution2D, Conv2D, MaxPooling2D, UpSampling2D
 from keras.layers.normalization import BatchNormalization
 from keras.models import Model
+from keras.optimizers import Adam
 from keras.utils import np_utils
+import os
+
+from src.loader import *
+from src.metrics import dice_coefficient, dice_coefficient_loss
+from src.normalize import denormalize_y
 
 # imageは(256, 256, 1)で読み込み
 IMAGE_SIZE = 256
@@ -69,6 +75,68 @@ def segnet(input_channel_count, output_channel_count, first_layer_filter_count):
     return model
 
 
+# train SegNet model
+def train_segnet():
+    # training/validation dataset paths
+    training_path = 'datasets' + os.sep + 'training'
+    validation_path = 'datasets' + os.sep + 'test'
+
+    # 訓練用imageデータ読み込み
+    x_train, file_names = load_x('datasets' + os.sep + 'training' + os.sep + 'image')
+    # 訓練用labelデータ読み込み
+    y_train = load_y('datasets' + os.sep + 'training' + os.sep + 'label')
+    # 検証用imageデータ読み込み
+    x_validation, file_names2 = load_x('datasets' + os.sep + 'test' + os.sep + 'image')
+    # 検証用labelデータ読み込み
+    y_validation = load_y('datasets' + os.sep + 'test' + os.sep + 'label')
+
+    # 入力はグレースケール1チャンネル
+    input_channel_count = 1
+    # 出力はグレースケール1チャンネル
+    output_channel_count = 1
+
+    # SegNetの定義
+    model = segnet(input_channel_count, output_channel_count, 64)
+
+    model.compile(loss=dice_coefficient_loss, optimizer=Adam(lr=1e-4), metrics=[dice_coefficient, 'accuracy'])
+
+    BATCH_SIZE = 8
+    # 20エポック回せば十分
+    NUM_EPOCH = 1
+
+    history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=NUM_EPOCH, verbose=1,
+                        validation_data=(x_validation, y_validation))
+    model.save_weights('segnet_weights.hdf5')
+
+    return history
+
+
 # prediction using SegNet
 def segnet_predict():
+    import cv2
+
+    rotation = False
+    # test内の画像で予測
+    X_test, file_names = load_x('datasets' + os.sep + 'test' + os.sep + 'image', rotation)
+
+    input_channel_count = 1
+    output_channel_count = 1
+    first_layer_filter_count = 64
+    model = segnet(input_channel_count, output_channel_count, first_layer_filter_count)
+    model.load_weights('segnet_weights.hdf5')
+    BATCH_SIZE = 12
+    Y_pred = model.predict(X_test, BATCH_SIZE)
+
+    for i, y in enumerate(Y_pred):
+        # testDataフォルダ配下にleft_imagesフォルダを置いている
+        img = cv2.imread('datasets' + os.sep + 'test' + os.sep + 'image' + os.sep + file_names[i], 0)
+
+        if rotation:
+            y = cv2.resize(y, (img.shape[0], img.shape[0]))
+        else:
+            y = cv2.resize(y, (img.shape[1], img.shape[0]))
+
+        y_dn = denormalize_y(y)
+        cv2.imwrite('prediction' + os.sep + file_names[i], y_dn)
+
     return 0
